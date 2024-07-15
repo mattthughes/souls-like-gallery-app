98ec3a84c31db973938482e2fd3dea51623d0076
Matthew Hughes <matt.h17@yahoo.com>
2024-07-15 21:33:31 +0000

    Added navigation bar links to nav component

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
       <Container className={styles.Main}>
       <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/Gallery" render={() => <h1>Gallery</h1>}/>
+          <Route exact path="/gallery" render={() => <h1>Gallery</h1>}/>
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
           <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
-          <Route exact path="/Profile" render={() => <h1>Profile</h1>}/>
-          <Route exact path="/Trending" render={()=> <h1>Trending</h1>}/>
+          <Route exact path="/profile" render={() => <h1>Profile</h1>}/>
+          <Route exact path="/trending" render={()=> <h1>Trending</h1>}/>
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       <NavBar/>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import logo from '../assets/logo.png'
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -11,9 +12,19 @@ const NavBar = () => {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto text-left">
-      <Nav.Link>Home</Nav.Link>
-      <Nav.Link>Sign in</Nav.Link>
-      <Nav.Link>Sign up</Nav.Link>
+      <NavLink
+      exact
+      to="/"
+      >Home
+      </NavLink>
+      <NavLink
+      to="/signin"
+      >Sign in
+      </NavLink>
+      <NavLink
+      to="/signup"
+      >Sign up
+      </NavLink>
     </Nav>
   </Navbar.Collapse>
   </Container>
