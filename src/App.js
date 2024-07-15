5b5547f626856d34fd66b1974f10d159397e6071
Matthew Hughes <matt.h17@yahoo.com>
2024-07-15 20:48:30 +0000

    Added placeholder navigation routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import NavBar from './components/NavBar';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import styles from './App.module.css';
+import { Container } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Container className={styles.Main}>
+      <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/Gallery" render={() => <h1>Gallery</h1>}/>
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/Profile" render={() => <h1>Profile</h1>}/>
+          <Route exact path="/Trending" render={()=> <h1>Trending</h1>}/>
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
       <NavBar/>
+      </Container>
+      
     </div>
   );
 }
