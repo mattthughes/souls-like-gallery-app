88db5f07d41b506d8c370d801e6020eefffce190
Matthew Hughes <matt.h17@yahoo.com>
2024-07-16 18:10:00 +0000

    Added current user context and sign in form component

--- src/api/AxiosDefaults.js
+++ src/api/AxiosDefaults.js
@@ -3,3 +3,5 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://souls-like-api-bc59577c0282.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosRes = axios.create();
