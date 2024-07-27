544bddbb69c4c11268a68f3cd20722e0f8c82655
Matthew Hughes <matt.h17@yahoo.com>
2024-07-27 16:50:54 +0000

    Adjusted padding value on post

--- src/pages/gallery/Post.js
+++ src/pages/gallery/Post.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { Card } from "react-bootstrap";
 
 import Image from "react-bootstrap/Image";
 
@@ -23,7 +22,7 @@ const Post = (props) => {
   return (
       <Link to={`/posts/${id}`}>
         
-      <Image height={150} width={150} className="col-6 col-lg-3 justify-content-lg-around pt-5 pl-2 pb-2 border border-dark mb-2 rounded-top" src={image} alt={title} />
+      <Image height={150} width={150} className="col-6 col-lg-3 justify-content-lg-around pt-4 pl-2 pb-2 border border-dark mb-2 rounded-top" src={image} alt={title} />
     </Link>
     
     
