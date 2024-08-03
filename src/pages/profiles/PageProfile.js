cd5a529e6d0307d11df33c650d2f84cfbabe5747
Matthew Hughes <matt.h17@yahoo.com>
2024-08-03 17:31:50 +0000

    Linked posts to account holder

--- src/pages/profiles/PageProfile.js
+++ src/pages/profiles/PageProfile.js
@@ -11,78 +11,103 @@ import appStyles from "../../App.module.css";
 
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/AxiosDefaults";
-import {  Image } from "react-bootstrap";
+import { Image } from "react-bootstrap";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import PostDetail from "../gallery/PostDetail";
+import {
+    fetchMoreData
+} from "../../utils/utils";
 
 function ProfilePage() {
-  const [hasLoaded, setHasLoaded] = useState(false);
-  const { id } = useParams();
-  const [profile, setProfileData] = useState({ results: [{}] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { id } = useParams();
+    const [profile, setProfileData] = useState({ results: [{}] });
+    const [profilePosts, setProfilePosts] = useState({ results: [] });
 
-  useEffect(() => {
-    const fetchProfileData = async () => {
-        try {
-            const [{ data: pageProfile }] = await Promise.all([
-                axiosReq.get(`/profiles/${id}/`),
-            ]);
-            setProfileData(pageProfile);
-            setHasLoaded(true);
-        } catch (err) {
-            console.log(err.response.data)
-        }
-    };
-    fetchProfileData();
-}, [id, setProfileData]);
+    useEffect(() => {
+        const fetchProfileData = async () => {
+            try {
+                const [{ data: pageProfile }, { data: profilePosts }] = await Promise.all([
+                    axiosReq.get(`/profiles/${id}/`),
+                    axiosReq.get(`/posts/?owner__profile=${id}`)
+                ]);
+                setProfilePosts(profilePosts);
+                setProfileData(pageProfile);
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err.response.data)
+            }
+        };
+        fetchProfileData();
+    }, [id, setProfileData]);
 
-  const mainProfile = (
-    <>
-      <Row noGutters className="px-3 text-center">
-        <Col lg={3} className="text-lg-left">
-          <Image
-            className={styles.ProfileImage}
-            roundedCircle
-            src={profile?.image}
-          />
-        </Col>
-        <Col lg={6}>
-          <h3 className="m-2">{profile?.owner}</h3>
-          <Row className="justify-content-center no-gutters">
-            <Col xs={3} className="my-2">
-              <div>{profile?.posts_count}</div>
-              <div>posts</div>
+    const mainProfile = (
+        <>
+            <Row noGutters className="px-3 text-center">
+                <Col lg={3} className="text-lg-left">
+                    <Image
+                        className={styles.ProfileImage}
+                        roundedCircle
+                        src={profile?.image}
+                    />
+                </Col>
+                <Col lg={6}>
+                    <h3 className="m-2">{profile?.owner}</h3>
+                    <Row className="justify-content-center no-gutters">
+                        <Col xs={3} className="my-2">
+                            <div>{profile?.posts_count}</div>
+                            <div>posts</div>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col lg={3} className="text-lg-right">
+                </Col>
+                {profile?.content && <Col className="p-3">{profile.content}</Col>}
+            </Row>
+        </>
+    );
+
+    const mainProfilePosts = (
+        <>
+            <hr />
+            <p className="text-center">Profile owner's posts</p>
+            <hr />
+            {profilePosts.length ? (
+                                <InfiniteScroll
+                                    children={profilePosts.map((post) => (
+                                        <PostDetail key={post.id} {...post} setPosts={setProfilePosts} />
+                                    ))}
+                                    dataLength={profilePosts.length}
+                                    loader={<Asset spinner />}
+                                    hasMore={!!profilePosts.next}
+                                    next={() => fetchMoreData(profilePosts, setProfilePosts)}
+                                />
+                            ) : (
+                                <Asset
+                                    message={`No results found, ${profile?.owner} hasn't posted yet.`}
+                                />
+                            )}
+        </>
+    );
+
+    return (
+        <Row>
+            <Col className="py-2 p-3 p-lg-2" lg={12}>
+                <Container className={appStyles.Content}>
+                    {hasLoaded ? (
+                        <>
+                            {mainProfile}
+                            {mainProfilePosts}
+                        </>
+                    ) : (
+                        <Asset spinner />
+                    )}
+                </Container>
             </Col>
-          </Row>
-        </Col>
-        <Col lg={3} className="text-lg-right">
-        </Col>
-        {profile?.content && <Col className="p-3">{profile.content}</Col>}
-      </Row>
-    </>
-  );
-
-  const mainProfilePosts = (
-    <>
-      <hr />
-      <p className="text-center">Profile owner's posts</p>
-      <hr />
-    </>
-  );
-
-  return (
-    <Row>
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <Container className={appStyles.Content}>
-          {hasLoaded ? (
-            <>
-              {mainProfile}
-              {mainProfilePosts}
-            </>
-          ) : (
-            <Asset spinner />
-          )}
-        </Container>
-      </Col>
-    </Row>
-  );
+        </Row>
+    );
 }
 
 export default ProfilePage;
