79296a9268f3da3df87985e73a6c76508912ec15
Matthew Hughes <matt.h17@yahoo.com>
2024-08-05 21:40:46 +0000

    Removed unused game code, adjusted li styling

--- src/pages/games/Game.js
+++ src/pages/games/Game.js
@@ -1,13 +1,6 @@
 import React from "react";
-import { useCurrentUser } from "../../contexts/UserCurrentContext";
 
-import { Card, Media } from "react-bootstrap";
-import { DropDown } from "../../components/DropDown";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-
-import { axiosRes } from "../../api/AxiosDefaults";
-
-import { toast } from "react-toastify";
+import { Card } from "react-bootstrap";
 
 import styles from '../../styles/Game.module.css'
 
@@ -17,45 +10,14 @@ import styles from '../../styles/Game.module.css'
 
 const Game = (props) => {
   const {
-    id,
-    owner,
     title,
-    gamePage
   } = props
-
-  const currentUser = useCurrentUser();
-  const is_owner = currentUser?.username === owner;
-
-  const history = useHistory();
-
-  const handleDelete = async () => {
-    try {
-      await axiosRes.delete(`/games/${id}/`);
-      toast.success("Game Deleted")
-      history.goBack();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleEdit = () => {
-    history.push(`/games/${id}/edit`);
-  };
 
 
   return (
-    <Card>
-      <Card.Body>
-        <Media>
-        
-          <div>
-            {is_owner && gamePage && (
-              <DropDown handleDelete={handleDelete} handleEdit={handleEdit}/>
-            )}
-          </div>
-        </Media>
-      </Card.Body>
-      <div className="col-6">
+    <Card className={styles.Card}>
+      
+      <div className="col-12">
         <ul>
           <li className={styles.Li}>{title}</li>
         </ul>
