58d382b2c625fb35e704dd7afd3930f414dd751b
Matthew Hughes <matt.h17@yahoo.com>
2024-08-06 18:47:19 +0000

    Added not found page basic styling

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import { Button } from 'react-bootstrap';
+import btnStyles from '../styles/Button.module.css'
+import appStyles from '../App.module.css'
 
 function NotFound() {
+  const history = useHistory();
   return (
-    <div>NotFound</div>
+    <div>
+      <h2 className={appStyles.Headings}>Error page not found</h2>
+      <p>The page you were looking for could not be found. Click the back button to return to the previous page you were on.</p>
+      <Button
+              className={`${btnStyles.Button} ${btnStyles.Blue}`}
+              onClick={() => history.goBack()}
+            >
+              Back
+            </Button>
+
+    </div>
   )
 }
 
